6c993ca81fdf401f131ccde43bf2e992b916d85f
hiruwana <114765343+hiruwana@users.noreply.github.com>
2022-09-30 23:07:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,25 +36,25 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
+global.vcardowner = ['94701115328'] //ur owner number
+global.ownername = "HIRUWA" //ur owner name
+global.ytname = "YT: hiruwa" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
+global.owner = ['94701115328']
+global.ownertag = '94701115328' //ur tag number
+global.botnumber = '94701115328' //Add your Bot Number For Log
+global.botname = 'HIRUWA' //ur bot name
+global.ig = "https://youtube.com/channel/UCGfc_0MYkfiGN_uzs5ZMOtg" //your theme url which will be displayed on whatsapp
+global.myweb = "https://youtube.com/channel/UCGfc_0MYkfiGN_uzs5ZMOtg" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
 global.linkz = 'https://youtu.be/uzMbW5v9QLE'
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
+global.reactmoji = "❤️" //ur menu react emoji
+global.themeemoji = "🎁" //ur theme emoji
+global.packname = "HIRUWA" //ur sticker watermark packname
+global.author = "HIRUWA" //ur sticker watermark author
 global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
